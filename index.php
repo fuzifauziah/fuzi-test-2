f83f09eead66dc22f429f2cbea8c8ce93d2357e5
fuzifauziah <fuzifauziahrpl2smkm@gmail.com>
2024-10-03 09:29:51 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur in voluptate quibusdam exercitationem autem dolores suscipit minus numquam quis distinctio quia minima placeat, alias nulla rerum at laborum fugiat accusantium?</p>
 </body>
 </html>
